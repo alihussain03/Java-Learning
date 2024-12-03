cd4919d6954ca579a28a0820bc4a66efb6cef98d
Ali Hussain <alihussain.personal3@gmail.com>
2024-12-03 10:54:35 +0200

    Update Map methods

--- Books/Java8InAction/src/chapter8/MapFactory.java
+++ Books/Java8InAction/src/chapter8/MapFactory.java
@@ -1,5 +1,6 @@
 package chapter8;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MapFactory {
@@ -16,5 +17,45 @@ public class MapFactory {
         System.out.println("---------------------------");
         System.out.println("For Each for map:");
         employees.forEach((key, value) -> System.out.println(key + " -- " + value));
+
+
+        System.out.println("---------------------------");
+        System.out.println("Example of Computer if absent");
+        Map<String, String> map = new HashMap<>();
+
+        map.put("apple", "fruit");
+        map.put("carrot", "vegetable");
+
+        // Use computeIfAbsent to compute value for key 'banana'
+        map.computeIfAbsent("banana", key -> "fruit");
+        System.out.println(map);
+
+        System.out.println("\nExample of Computer if present");
+        map.computeIfPresent("banana", (key, value) -> value + 2);
+        System.out.println(map);
+
+        System.out.println("\n---------------------------");
+        System.out.println("Example of remove");
+        map.remove("banana");
+        System.out.println(map);
+
+        System.out.println("\n---------------------------");
+        System.out.println("Example of replaceAll");
+        map.replaceAll((s, s2) -> s2.toUpperCase());
+        System.out.println(map);
+
+        System.out.println("\n---------------------------");
+        System.out.println("Example of replace");
+        map.replace("apple", "fruit", "FRUIT");
+        System.out.println(map);
+
+        System.out.println("\n---------------------------");
+        System.out.println("Example of putAll");
+
+        Map<String, String> javaOld = Map.ofEntries(Map.entry("Java8", "2020"), Map.entry("Java7", "2017"));
+        Map<String, String> javaLatest = Map.ofEntries(Map.entry("Java11", "2022"), Map.entry("Java21", "2024"));
+        Map<String, String> java = new HashMap<>(javaOld);
+        java.putAll(javaLatest);
+        System.out.println(java);
     }
 }
